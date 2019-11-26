de0038f57f6d730730db6716c5a2d05686ed23db
GaoGN517 <luyeqing517@gmail.com>
2019-11-26 17:15:52 -0600

    ## Update documentation for vcf2df

--- R/vcf2df.R
+++ R/vcf2df.R
@@ -4,18 +4,53 @@
 #' generate the xgboost dataframe.
 #'
 #' @return A dataframe which we need to generate the xgboost data structure. 
+#' 
+#' @details 
+#' The input vcf file is directly from raw sequencing data.
+#' It contains (n + 9) columns (Information for each SNP and corresponding SNP values for the samples)
+#' and p rows (SNP positions). 
+#' 
+#' Starting from the 10th column are the information for the first sample. 
+#' So we first remove the first 9 columns.
+#' 
+#' For our imputation, we need the p SNPs as features (columns), n samples as rows, so we need
+#' to transpose the dataframe. 
+#' 
+#' In the input dataset, there are 2 values indicating the SNP types for each SNP position as there
+#' are two alleles:
+#' 0 (Wild type) and 1 (Mutate type). So the values can be "0/0", "0/1", "1/0", "1/1". Some of the
+#' values might be missing. 
+#' 
+#' We sum up the two values at each position to one value to represent the corresponding SNP type. 
+#' 
+#' In the output data, each unit is the corresponding SNP type:
+#' (1) 0: both alleles are mutations;
+#' (2) 1: one of the alleles is a mutation, the other is wild type;
+#' (3) 2: both alleles are wild type; 
+#' (4) NA: at least one of the SNP type of the two alleles is missing. We need to predict the value
+#'     for this position. 
+#' 
+#' 
 #' @export
 #'
 #' @examples
 #' filename <- "Test.vcf"
 #' output_df <- vcf2df(filename)
+#' ## This dataset has 112 samples and 338 SNP positions.
+#' ## The original file has 121 columns and 338 rows.
 #' 
+#' ## Output should be a dataset with 112 rows and 338 columns. 
 #' 
+
 vcf2df <- function(filename) {
   ## Read in the original vcf file as a dataframe.
   df <- read.table(filename)
-  ## Greb information from the original dataframe.
+  
+  ## Subset SNP type related information starting from the 10th column of the original dataframe.
+  ## Transpose the dataframe so that SNPs are columns and samples are rows. 
   df_gt <- t(df[,10:ncol(df)]) 
+  
+  ## Greb the 0/1 values for SNP information
   df_gt <- gsub(":.*","",df_gt)
   df_gt <- gsub("\\|","/",df_gt)
   df_gt <- gsub("/"," ",df_gt)
