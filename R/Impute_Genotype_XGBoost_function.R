7737ec38ce37ce26f5658e10c0f7febfe08bcce5
GaoGN517 <luyeqing517@gmail.com>
2019-12-03 16:37:46 -0600

    Revert "## Add function to randomly introduce additional NAs to our data for tests."
    This reverts commit dd7279de2ee30c2e3049d07877a846e5dd7bc1d8.

--- R/Impute_Genotype_XGBoost_function.R
+++ R/Impute_Genotype_XGBoost_function.R
@@ -2,12 +2,10 @@
 #'
 #' @param df The original dataset including the missing SNPs to be imputed.    
 #' @param size The windows size to use as the training dataset for each SNP, default: 10.   
-#' @param nrounds Number of fitting rounds, default: 100. 
-#' @param num_class Number of classes of response variable (types of SNPs), default: 3.  
-#' 
-#' params A list of parameters for the xgboost model building. 
+#' @param params A list of parameters for the xgboost model building. 
 #' Default: nrounds = 100, booster = "gbtree", objective = "multi:softprob", 
 #' num_class = 3, eval_metric = "mlogloss".   
+#' @param df_test The dataset for testing model by calculating the prediction error. Default: NULL.
 #'    
 #' 
 #' @details 
@@ -21,7 +19,7 @@
 #' @examples
 #' df <- readRDS("Test_df.rds")
 #' params <- list(nrounds = 100, booster = "gbtree", objective = "multi:softprob", num_class = 3, eval_metric = "mlogloss")
-#' predict_df <- Impute_GenoType_XGBoost(df, size = 10, nrounds = 50)
+#' predict_df <- Impute_GenoType_XGBoost(df)
 #' ## May take several seconds to finish.
 #' ## Should return a dataset where the missing values are filled by predicted values.  
 #' 
@@ -34,7 +32,7 @@
 #' Tianqi Chen and Carlos Guestrin, "XGBoost: A Scalable Tree Boosting System",
 #' 22nd SIGKDD Conference on Knowledge Discovery and Data Mining, 2016, \url{https://arxiv.org/abs/1603.02754}
 #' 
-Impute_GenoType_XGBoost <- function(df, size = 10, num_class = 3, nrounds = 100){
+Impute_GenoType_XGBoost <- function(df, size = 10, params, df_test = NULL){
   ## To save time, we only impute SNP columns with NAs in the dataset.
   ## Get the dimension of df
   n <- nrow(df)
@@ -54,7 +52,7 @@ Impute_GenoType_XGBoost <- function(df, size = 10, num_class = 3, nrounds = 100)
     df_fill <- df
     error_vec <- rep(NA, n_NA_cols)
     
-    for(i in NA_cols) {
+    for(i in 1:n_NA_cols) {
       single_SNP_Obj <- Create_Single_SNP_Object(df = df, a = i, size = size)
       train_xgboost <- xgboost::xgb.DMatrix(data = single_SNP_Obj$train_data, label = single_SNP_Obj$train_label)
       pred_xgboost <- xgboost::xgb.DMatrix(data = single_SNP_Obj$pred_data, label = single_SNP_Obj$pred_label)
